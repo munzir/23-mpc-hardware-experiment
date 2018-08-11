fce49bb957f760ddcfc87aa771b0517f68d82e27
shimin zhang <szhang472@gatech.edu>
2018-08-11 00:26:21 -0400

    change common library path to absolute, update cmake version

--- build/CMakeFiles/00-balance.run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/00-balance.run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/00-balance.run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
